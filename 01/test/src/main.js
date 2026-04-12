a1b22021f58c9a6ec3243ae6401d2f656f2ee221
takagino <hirotaka.takagi@trident.ac.jp>
2026-04-12 16:38:05 +0900

    0412

---
+++ 01/test/src/main.js
@@ -0,0 +1,52 @@
+import './style.css';
+import * as THREE from 'three';
+
+// 1. シーンの作成
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#336699');
+
+// 2. カメラの作成
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+
+// 3. レンダラーの作成
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+// 4. ジオメトリ（形状）の作成： 幅1, 高さ1, 奥行き1の立方体
+const geometry = new THREE.CapsuleGeometry(1, 1, 4, 8, 1);
+
+// 5. マテリアル（質感）の作成： 光がなくても立体感がわかるノーマルマテリアル
+const material = new THREE.MeshNormalMaterial();
+
+// 6. メッシュで組み合わせる（形状 + 質感）
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// 7. アニメーション（毎フレーム更新）
+const update = () => {
+  window.requestAnimationFrame(update);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+update();
+
+// 8. ウィンドウのリサイズに対応
+const onWindowResize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+};
+
+window.addEventListener('resize', onWindowResize);
